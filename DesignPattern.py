fe3971e1abd5af33e9f62940e49ddfc096922437
Kishanbhatk <42643922+Kishanbhatk@users.noreply.github.com>
2021-08-24 17:31:43 +0530

    Update DesignPattern.py

--- DesignPattern.py
+++ DesignPattern.py
@@ -5,35 +5,39 @@ val = list(map(int,input().split()))
 lines = val[0]
 total = val[1]
 su = 0
-def dot(num):
-    for m in range(0,num):
-        print("-",end='')
-def pat(num):
-    for k in range(0,num):
-        if k % 2 == 0:
-            print(".",end='')
-        else:
-            print("|",end='')
-
 for i in range(0,lines):
     if i!=((lines/2)-1) and i<((lines/2)-1):
         add = 6*i
         j = int((total - (3+add))/2)
-        dot(j)
-        pat(3+add)
-        dot(j)
+        for m in range(0,j):
+            print("-",end='')
+        for k in range(0,3+add):
+            if (k -1 ) % 3 == 0:
+                print("|",end='')
+            else:
+                print(".",end='')
+        for m in range(0,j):
+            print("-",end='')
         su = su + 1
     elif i == int((lines/2)):
         j = int((total-7)/2)
-        dot(j)
+        for m in range(0,j):
+            print("-",end='')
         for k in "WELCOME":
             print(k,end='')
-        dot(j)
+        for m in range(0,j):
+            print("-",end='')
     else:
         add = 6*(su-1)
         j = int((total-(3+add))/2)
-        dot(j)
-        pat(3+add)
-        dot(j)
+        for m in range(0,j):
+            print("-",end='')
+        for k in range(0,3+add):
+            if (k -1) % 3 ==0:
+                print("|",end='')
+            else:
+                print(".",end='')
+        for m in range(0,j):
+            print("-",end='')
         su = su - 1
     print()        
